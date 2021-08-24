3cf804b2266ba26a2b80b69a7f07dae626ce7295
Lee <lee.n@compute1-client-3.ris.wustl.edu>
2021-08-24 16:50:33 -0500

    added rp pipe file

---
+++ scripts/rp_pipe.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+# 1. get as input: fastq file directory                                                                                            # 2. generate output directory                                                                                                     # 3. perform bbduk << consider incorporating bbduk directly into the script                                                        # 4. loop for each barcode: generate output directory > perform mapping for both cellular & viral   
